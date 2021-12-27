549c6077b578ec827d34520780d1128cb6b7c887
Freewind <nowindlee@gmail.com>
2021-12-27 16:06:42 +0800

    update versions to latest and simplify

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,9 +1,11 @@
 package demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println(StringUtils.trim("Hello World!  "));
+  }
 
 }
